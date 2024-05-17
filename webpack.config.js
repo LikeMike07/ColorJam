82997839dd776b0af264cd97efa8c4c4e40217fc
Michael Raisch <mraisc07@gmail.com>
2024-05-16 19:31:58 -0700

    all you need to start a ts-react app with webpack :)

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const path = require('path')
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [],
+};
